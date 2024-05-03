c2ac6eef83ede1defcb6fb4b3381086d53aa167a
NeilRow56 <neilrowland56@gmail.com>
2024-05-03 05:06:03 +0100

    Updated middleware

--- middleware.ts
+++ middleware.ts
@@ -1,18 +1,18 @@
-import { withAuth } from 'next-auth/middleware'
+// import { withAuth } from 'next-auth/middleware'
 
-export default withAuth({
-  callbacks: {
-    authorized({ req, token }) {
-      const isLoggedIn = !!token
-      const isOnDashboard = req.nextUrl.pathname.startsWith('/')
-      if (isOnDashboard) {
-        if (isLoggedIn) return true
-        return false // Redirect unauthenticated users to login page
-      }
-      return true
-    },
-  },
-})
+// export default withAuth({
+//   callbacks: {
+//     authorized({ req, token }) {
+//       const isLoggedIn = !!token
+//       const isOnDashboard = req.nextUrl.pathname.startsWith('/')
+//       if (isOnDashboard) {
+//         if (isLoggedIn) return true
+//         return false // Redirect unauthenticated users to login page
+//       }
+//       return true
+//     },
+//   },
+// })
 
 export const config = {
   // https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
