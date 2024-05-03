2d422034605ffd014df73c36c1a661ad23143b05
NeilRow56 <neilrowland56@gmail.com>
2024-05-03 08:04:19 +0100

    Added tanstack query and user-settings api

---
+++ app/api/user-settings/route.ts
@@ -0,0 +1,32 @@
+import { auth } from '@/lib/auth'
+import db from '@/lib/db'
+import { revalidatePath } from 'next/cache'
+import { redirect } from 'next/navigation'
+
+export async function GET(request: Request) {
+  const session = await auth()
+  const user = session?.user
+
+  if (!user) {
+    redirect('/auth/sign-in')
+  }
+
+  let userSettings = await db.userSettings.findUnique({
+    where: {
+      id: user.id,
+    },
+  })
+
+  if (!userSettings) {
+    userSettings = await db.userSettings.create({
+      data: {
+        userId: user.id,
+        currency: 'GBP',
+      },
+    })
+  }
+
+  // Revalidate the home page that uses the user currency
+  revalidatePath('/dashboard')
+  return Response.json(userSettings)
+}
