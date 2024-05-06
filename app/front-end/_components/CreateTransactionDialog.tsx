fad4d6814e18c043257a4469da705ddb680bfa4a
NeilRow56 <neilrowland56@gmail.com>
2024-05-06 14:36:44 +0100

    Tweak to create transaction

--- app/front-end/_components/CreateTransactionDialog.tsx
+++ app/front-end/_components/CreateTransactionDialog.tsx
@@ -211,7 +211,10 @@ function CreateTransactionDialog({ trigger, type }: TransactionProps) {
                         <Calendar
                           mode="single"
                           selected={field.value}
-                          onSelect={field.onChange}
+                          onSelect={(value) => {
+                            if (!value) return
+                            field.onChange(value)
+                          }}
                           initialFocus
                         />
                       </PopoverContent>
