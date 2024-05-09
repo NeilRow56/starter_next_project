819f8ddab564501c349b85a542e611fdabba32c9
NeilRow56 <neilrowland56@gmail.com>
2024-05-09 07:43:15 +0100

    Remaining columns added to table

--- app/front-end/_components/TransactionTable.tsx
+++ app/front-end/_components/TransactionTable.tsx
@@ -26,6 +26,7 @@ import { useQuery } from '@tanstack/react-query'
 import React, { useState } from 'react'
 import SkeletonWrapper from '@/components/SkeletonWrapper'
 import { DataTableColumnHeader } from '@/components/datatable/ColumnHeader'
+import { cn } from '@/lib/utils'
 
 interface Props {
   from: Date
@@ -48,6 +49,62 @@ const columns: ColumnDef<TransactionHistoryRow>[] = [
         {row.original.categoryIcon}
         <div className="capitalize">{row.original.category}</div>
       </div>
+    ),
+  },
+  {
+    accessorKey: 'description',
+    header: ({ column }) => (
+      <DataTableColumnHeader column={column} title="Description" />
+    ),
+
+    cell: ({ row }) => (
+      <div className="capitalize">{row.original.description}</div>
+    ),
+  },
+  {
+    accessorKey: 'date',
+    header: 'Date',
+    cell: ({ row }) => {
+      const date = new Date(row.original.date)
+      const formattedDate = date.toLocaleDateString('default', {
+        timeZone: 'UTC',
+        year: 'numeric',
+        month: '2-digit',
+        day: '2-digit',
+      })
+      return <div className="text-muted-foreground">{formattedDate}</div>
+    },
+  },
+  {
+    accessorKey: 'type',
+    header: ({ column }) => (
+      <DataTableColumnHeader column={column} title="Type" />
+    ),
+    filterFn: (row, id, value) => {
+      return value.includes(row.getValue(id))
+    },
+    cell: ({ row }) => (
+      <div
+        className={cn(
+          'rounded-lg p-2 text-center capitalize',
+          row.original.type === 'income' &&
+            'bg-emerald-400/10 text-emerald-500',
+          row.original.type === 'expense' && 'bg-red-400/10 text-red-500'
+        )}
+      >
+        {row.original.type}
+      </div>
+    ),
+  },
+  {
+    accessorKey: 'amount',
+    header: ({ column }) => (
+      <DataTableColumnHeader column={column} title="Amount" />
+    ),
+    cell: ({ row }) => (
+      <p className="text-md rounded-lg bg-gray-400/5 p-2 text-right font-medium">
+        {row.original.formattedAmount}
+      </p>
     ),
   },
 ]
